0807c531232c2a5cdf5672481fbc9428f1e6d541
Uros Kozoderovic <kozoderovicu@gmail.com>
2026-09-14 13:14:26 +0200

    another device debug

--- FarmlandProject/obj/Release/net10.0/FarmlandProject.AssemblyInfo.cs
+++ FarmlandProject/obj/Release/net10.0/FarmlandProject.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FarmlandProject")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea4abb77b59c1be33d4196d75ccb17af418a2017")]
 [assembly: System.Reflection.AssemblyProductAttribute("FarmlandProject")]
