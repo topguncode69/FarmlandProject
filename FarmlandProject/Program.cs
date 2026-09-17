be5350a7c2af875645a296b086e972a0a37e39fb
Uros Kozoderovic <kozoderovicu@gmail.com>
2026-09-17 19:01:31 +0200

    added a better OOP structure, player class

--- FarmlandProject/Program.cs
+++ FarmlandProject/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace FarmlandProject
 {
@@ -8,12 +8,8 @@ namespace FarmlandProject
         {
             try
             {
-                var time = new Time();
-                var crop = new Crop(time);
-                var money = new Money();
-                var farm = new Farm(crop, money, time);
-                
-
+                var player = new Player();
+               
                 bool exit = false;
                 while (!exit)
                 {
@@ -40,26 +36,26 @@ namespace FarmlandProject
                     switch (userInput)
                     {
                         case 1:
-                            crop.Plant();
+                            player.crop.Plant();
                             break;
                         case 2:
-                            crop.Harvest();
+                            player.crop.Harvest();
                             break;
                         case 3:
-                            money.BuyAnimal();
+                            player.money.BuyAnimal();
                             break;
                         case 4:
-                            money.SellAnimal();
+                            player.money.SellAnimal();
                             break;
                         case 5:
-                            farm.ShowStatus();
+                            player.farm.ShowStatus();
                             break;
                         case 6:
                             Console.WriteLine("Exiting program. Goodbye!");
                             exit = true;
                             break;
                         case 7:
-                            time.Tick();
+                            player.time.Tick();
                             Console.WriteLine("Good night, sweet dreams..");
                             break;
                         default:
@@ -82,8 +78,28 @@ namespace FarmlandProject
             }
         }
     }
-    public class Time // also sleep
-    { 
+   
+    public class Player
+    {
+        public Time time { get; private set; }
+        public Crop crop { get; private set; }
+        public Money money { get; private set; }
+        public Farm farm { get; private set; }
+
+
+        public Player()
+        {
+            // Initialize in correct order: Time first, then Crop (which needs Time)
+            time = new Time();
+            crop = new Crop(time);
+            money = new Money();
+            farm = new Farm(crop, money, time);
+
+        }
+   
+
+         public class Time // also sleep
+    {
         private int _day = 0;
         public int Day => _day;
         public void Tick()
@@ -91,111 +107,110 @@ namespace FarmlandProject
             _day += 1;
         }
     }
-    public class Farm // the farm class with the showstatus
-    {
-        private readonly Crop _crop;
-        private readonly Money _animal;
-        private readonly Money _totalGold;
-        private readonly Time _time1;
-        public Farm(Crop crop, Money money, Time time2)
+        public class Farm // the farm class with the showstatus
         {
-            _crop = crop ?? throw new ArgumentNullException(nameof(crop));
-            _animal = money ?? throw new ArgumentNullException(nameof(money));
-            _totalGold = money ?? throw new ArgumentNullException(nameof(money));
-            _time1 = time2 ?? throw new ArgumentNullException(nameof(time2));
-        }
-
-        public void ShowStatus()
-        {
-            Console.WriteLine("Showing farm status...");
-            Console.WriteLine($"Planted = {_crop.Planted}");
-            Console.WriteLine($"Harvested = {_crop.Harvested}");
-            Console.WriteLine($"Animals = {_animal.Animals}");
-            Console.WriteLine($"Money = {_totalGold.TotalGold} Gold");
-            Console.WriteLine($"Day = {_time1.Day}");
-
-        }
-    }
-
-    public class Crop // crop class with plant and harvest
-    {
-        private int _planted = 0;
-        private int _harvested = 0;
-        private bool _grown = false;
-
-
-        public int _dayPlanted;
-        public int Planted => _planted;
-        public int Harvested => _harvested;
-        public bool Grown => _grown;
-        
-        
-        
-        private readonly Time _time;
-        public Crop(Time time)
-        {
-            _time = time ?? throw new ArgumentNullException(nameof(time));
-        }  // TIME SYSTEM
-
-        public void Plant()
-        {
-            _planted++;
-            Console.WriteLine("You have planted a crop.");
-            _dayPlanted = _time.Day; // assigning to a global day so the game remembers
-            _grown = false;
-
-        }
-
-        public void Harvest()
-        {
-            if (_planted < 1) 
+            private readonly Crop _crop;
+            private readonly Money _animal;
+            private readonly Money _totalGold;
+            private readonly Time _time1;
+            public Farm(Crop crop, Money money, Time time2)
             {
-                Console.WriteLine("You have no planted crops to harvest.");
-                return;
+                _crop = crop ?? throw new ArgumentNullException(nameof(crop));
+                _animal = money ?? throw new ArgumentNullException(nameof(money));
+                _totalGold = money ?? throw new ArgumentNullException(nameof(money));
+                _time1 = time2 ?? throw new ArgumentNullException(nameof(time2));
             }
-            if (_time.Day <= _dayPlanted + 3)
+
+            public void ShowStatus()
             {
-                Console.WriteLine("The plant has not grown yet.");
-                return;
+                Console.WriteLine("Showing farm status...");
+                Console.WriteLine($"Planted = {_crop.Planted}");
+                Console.WriteLine($"Harvested = {_crop.Harvested}");
+                Console.WriteLine($"Animals = {_animal.Animals}");
+                Console.WriteLine($"Money = {_totalGold.TotalGold} Gold");
+                Console.WriteLine($"Day = {_time1.Day}");
+
             }
-          
-            
-            _planted--;
-            _harvested++;
-            Console.WriteLine("You have harvested a crop.");
         }
 
-
-    }
-
-    public class Money 
-    {
-        private int _animals = 0;
-        public int Animals => _animals;
-
-        public void BuyAnimal()
+        public class Crop // crop class with plant and harvest
         {
-            _animals++;
-            Console.WriteLine("An animal has been bought.");
-        }
-        private int _totalGold = 0;
+            private int _planted = 0;
+            private int _harvested = 0;
+            private bool _grown = false;
 
-        public int TotalGold => _totalGold;
-
-        public void SellAnimal()
-        {
-            if (_animals > 0)
+            public int _dayPlanted;
+            public int Planted => _planted;
+            public int Harvested => _harvested;
+            public bool Grown => _grown;
+            private readonly Time _time;
+            public Crop(Time time)
             {
-                _totalGold+= 150;
+                _time = time ?? throw new ArgumentNullException(nameof(time));
+
+            }  // TIME SYSTEM
+
+
+            public void Plant()
+            {
+                _planted++;
+                Console.WriteLine("You have planted a crop.");
+                _dayPlanted = _time.Day; // assigning to a global day so the game remembers
+                _grown = false;
+            }
+
+            public void Harvest()
+            {
+                if (_planted < 1)
+                {
+                    Console.WriteLine("You have no planted crops to harvest.");
+                    return;
+                }
+                if (_time.Day <= _dayPlanted + 3)
+                {
+                    Console.WriteLine("The plant has not grown yet.");
+                    return;
+                }
+
+
+                _planted--;
+                _harvested++;
+                Console.WriteLine("You have harvested a crop.");
+            }
+
+
+        }
+
+        public class Money
+        {
+            private int _animals = 0;
+            public int Animals => _animals;
+
+            public void BuyAnimal()
+            {
+                _animals++;
+                Console.WriteLine("An animal has been bought.");
+            }
+            private int _totalGold = 0;
+
+            public int TotalGold => _totalGold;
+
+            public void SellAnimal()
+            {
+                if (_animals > 0)
+                {
+                    _totalGold += 150;
                     _animals--;
-                Console.WriteLine("An animal has been sold");
-            } else
-            {
-                Console.WriteLine("You have no Animals to sell.");
+                    Console.WriteLine("An animal has been sold");
+                }
+                else
+                {
+                    Console.WriteLine("You have no Animals to sell.");
+                }
             }
-        }
 
-        
+
+        }
     }
 }
 
